37710f458c24b00c72486c13c28f0b6f08636d98
Joachim Penserot <vonpenserot@192.168.1.7>
2023-03-25 18:31:20 -0400

    first commit

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,55 @@
+import { ScrollView, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { Participant } from "../components/Participant";
+
+
+import { styles } from "./style";
+
+export function Home() {
+  const participants = ['Rodrigo', 'Vini', 'Diego', 'Biro', 'Ana', 'Isa', 'Jack', 'Mayk', 'João', ""];
+  
+  function handleParticipantAdd() {
+    console.log("Você clicou no botão de Adicionar!");
+  }
+
+  function handleParticipantRemove(name: string) {
+    console.log(`Você clicou em remover o participante ${name}`);
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.eventName}>
+        Nome do evento
+      </Text>
+
+      <Text style={styles.eventDate}>
+        Sexta, 4 de Novembro de 2022.
+      </Text>
+
+      <View style={styles.form}>
+        <TextInput 
+          style={styles.input}
+          placeholder="Nome do participante"
+          placeholderTextColor="#6B6B6B"
+        />
+        
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+          <Text style={styles.buttonText}>
+            +
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <ScrollView showsVerticalScrollIndicator={false}>
+        {
+          participants.map(participant => (
+            <Participant 
+              key={participant} 
+              name={participant} 
+              onRemove={() => handleParticipantRemove("Rodrigo")} 
+            />
+          ))
+        }
+      </ScrollView>
+    </View>
+  )
+}
